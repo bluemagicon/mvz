f6ba321f832653c8cc24556328f7d74cf10f998c
peverts <phillip@deux.de>
2021-06-11 09:17:44 +0200

    Footer+Header

--- footer.php
+++ footer.php
@@ -30,50 +30,68 @@ $page_support	= $override_support ?: $global_support;
 
 <footer class="footer">
 	<div class="footer-inner">
-		<div class="uk-grid uk-grid-large uk-flex-between">
-
-
-			<?php if($page_footer['show_contact'] && $global_contact) { ?>
-				<div class="uk-width-1-2@m uk-width-auto@l">
-					<div class="footer-item footer-contact">
-						<?php include 'tpl/partials/contact.php'; ?>
-					</div>
-				</div>
-			<?php } ?>
-
-
-			<?php if($page_footer['footer_text']) { ?>
-				<div class="uk-width-1-2@m uk-width-1-3@l">
-					<div class="footer-item footer-text">
-						<?= $global_footer['footer_text'] ?>
-					</div>
-				</div>
-			<?php } ?>
-
-
-			<div class="uk-width-auto@l uk-text-right@l">
-				<?php if($page_footer['show_logo'] && $page_logos) { ?>
-					<div class="footer-item footer-logo">
-						<?php include_once 'tpl/partials/logo-function.php'; ?>
-						<?php include 'tpl/partials/logo.php'; ?>
-					</div>
-				<?php } ?>
-				<?php if(has_nav_menu('footer')) { ?>
-					<div><div class="footer-item footer-menu">
-						<?php wp_nav_menu(array('theme_location' => 'footer', 'container' => false, 'fallback_cb' => false)); ?>
-					</div></div>
-				<?php } ?>
-				<?php if($page_footer['show_socialmedia'] && $global_social) { ?>
-					<div><div class="footer-item footer-social">
-						<?php $social_profiles = $global_social;
-						include 'tpl/partials/social.php'; ?>
-					</div></div>
-				<?php } ?>
-			</div>
-
-
-		</div>
-	</div>
+        <div class="footer-col">
+            <?php echo wp_nav_menu(array('menu' => 'Hauptmenü')); ?>
+        </div>
+        <div class="footer-col">
+            <p>
+                <strong>MVZ Bremen-Mitte</strong> <br />
+                Außer der Schleifmühle 64/66 <br />
+                28203 Bremen
+            </p>
+            <a href="#" class="button-plain">
+                <svg xmlns="http://www.w3.org/2000/svg" width="8.496" height="14" viewBox="0 0 8.496 14">
+                    <path id="FontAwsome_chevron-right_" data-name="FontAwsome (chevron-right)" d="M35.765,45.2l-6.231,6.231a.769.769,0,0,1-1.088,0l-.727-.727a.769.769,0,0,1,0-1.087l4.938-4.961L27.719,39.7a.769.769,0,0,1,0-1.087l.727-.727a.769.769,0,0,1,1.088,0l6.231,6.231A.769.769,0,0,1,35.765,45.2Z" transform="translate(-27.494 -37.657)" fill="#143a89"/>
+                </svg>
+                Anfahrt, ÖVB und Parken
+            </a>
+            <div class="flex-wrapper">
+                <div class="col">
+                    <p>
+                        <strong>Innere Medizin</strong> <br />
+                        0421 - 323323 <br />
+                    </p>
+                    <a href="#" class="button-plain">
+                        <svg xmlns="http://www.w3.org/2000/svg" width="8.496" height="14" viewBox="0 0 8.496 14">
+                            <path id="FontAwsome_chevron-right_" data-name="FontAwsome (chevron-right)" d="M35.765,45.2l-6.231,6.231a.769.769,0,0,1-1.088,0l-.727-.727a.769.769,0,0,1,0-1.087l4.938-4.961L27.719,39.7a.769.769,0,0,1,0-1.087l.727-.727a.769.769,0,0,1,1.088,0l6.231,6.231A.769.769,0,0,1,35.765,45.2Z" transform="translate(-27.494 -37.657)" fill="#143a89"/>
+                        </svg>
+                        Jetzt anrufen
+                    </a>
+                </div>
+                <div class="col">
+                    <p>
+                        <strong>Gynäkoligie</strong> <br />
+                        0421 - 704446<br/>
+                    </p>
+                    <a href="#" class="button-plain">
+                        <svg xmlns="http://www.w3.org/2000/svg" width="8.496" height="14" viewBox="0 0 8.496 14">
+                            <path id="FontAwsome_chevron-right_" data-name="FontAwsome (chevron-right)" d="M35.765,45.2l-6.231,6.231a.769.769,0,0,1-1.088,0l-.727-.727a.769.769,0,0,1,0-1.087l4.938-4.961L27.719,39.7a.769.769,0,0,1,0-1.087l.727-.727a.769.769,0,0,1,1.088,0l6.231,6.231A.769.769,0,0,1,35.765,45.2Z" transform="translate(-27.494 -37.657)" fill="#143a89"/>
+                        </svg>
+                        Jetzt anrufen
+                    </a>
+                </div>
+                <div class="button-wrapper">
+                    <span class="ghostkit-button ghostkit-button-md ghostkit-custom-Z1aV2wz">
+                        <span class="ghostkit-button-icon ghostkit-button-icon-left">
+                            <svg class="ghostkit-svg-icon" width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" clip-rule="evenodd" d="M9.21967 6.2197C9.51256 5.9268 9.98744 5.9268 10.2803 6.2197L15.5303 11.4697C15.8232 11.7626 15.8232 12.2374 15.5303 12.5303L10.2803 17.7803C9.98744 18.0732 9.51256 18.0732 9.21967 17.7803C8.92678 17.4874 8.92678 17.0126 9.21967 16.7197L13.9393 12L9.21967 7.2803C8.92678 6.9874 8.92678 6.5126 9.21967 6.2197Z" fill="currentColor"></path>
+                            </svg>
+                        </span>
+                        <span class="ghostkit-button-text">E-MAIL SCHREIBEN</span>
+                    </span>
+                </div>
+            </div>
+        </div>
+        <div class="footer-col">
+            CONNECTOR
+        </div>
+        <div class="footer-col">
+            <?php include_once 'tpl/partials/logo-function.php'; ?>
+            <?php include 'tpl/partials/logo.php'; ?>
+        </div>
+        <div class="meta-menu">
+            <?php echo wp_nav_menu(array('menu' => 'Footer')); ?>
+        </div>
+    </div>
 </footer>
 
 <?php
